d42ddda1bc501cdbe45da164c7e54b9322c9dabe
Paramveerjamhal <paramveerjamhal@gmail.com>
2018-08-09 00:23:05 -0400

    param git initial setup

--- app/src/main/java/com/example/paramveerjamhal/food4kids/entities/UserWithEventTask.java
+++ app/src/main/java/com/example/paramveerjamhal/food4kids/entities/UserWithEventTask.java
@@ -2,7 +2,7 @@ package com.example.paramveerjamhal.food4kids.entities;
 
 import java.io.Serializable;
 
-public class UserWithEventTAsk implements Serializable {
+public class UserWithEventTask implements Serializable {
     int id;
     String name;
     String email;
@@ -22,7 +22,7 @@ public class UserWithEventTAsk implements Serializable {
 
 
 
-    public UserWithEventTAsk(int id,int w_event_id, int event_id, String date,int participate_id,
+    public UserWithEventTask(int id,int w_event_id, int event_id, String date,int participate_id,
                              String user_startTime, String user_endTime, int admin_approveStatus,String name,
                              String email,String mobile,String address,String city,String postal_code) {
         this.id=id;
